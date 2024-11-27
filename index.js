7353e93c752869e958475b9264379d697fba0b44
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-27 05:30:32 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config()
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"})
